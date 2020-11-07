b10aaed7137f8146b1cb5b6ddce3ae105d53d076
Marshall Ferguson <moferg01@louisville.edu>
2020-11-07 06:41:55 -0500

    added outline code for bot

---
+++ bot.py
@@ -0,0 +1,20 @@
+# Marshall Ferguson - 11/2020
+
+# Imports
+
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+bot.run(TOKEN)
