291cd004e9249ad568b4f5267dbb2e6015f8d6fd
Marshall Ferguson <moferg01@louisville.edu>
2020-11-08 09:42:18 -0500

    added sarcasm command

--- bot.py
+++ bot.py
@@ -16,4 +16,21 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
+@bot.command(help="i MaKe FuN oF pEoPlE sO yOu dOn'T hAvE tO. tYpE '!sarcasm' fOlLoWeD bY tHe TeXt To MiMiC")
+async def sarcasm(ctx, *, message):
+    # message = await ctx.fetch_message(ctx.channel.last_message_id)
+    # if message.author == bot.user:
+    #     return
+
+    sarcatic_message = ''
+    condition = True
+    for letter in message:
+        if condition:
+            sarcatic_message += letter.upper()
+        else:
+            sarcatic_message += letter.lower()
+        if letter != ' ':
+            condition = not condition
+    await ctx.send(sarcatic_message)
+
 bot.run(TOKEN)
